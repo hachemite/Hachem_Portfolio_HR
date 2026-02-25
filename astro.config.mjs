4ca65b1d5e8a63f9910cef4189a55f8f6cd2480e
hachemite Squalli dodoracula <66376987+hachemite@users.noreply.github.com>
2026-02-25 00:35:12 +0000

    feat(image) : add image and grid for webstie index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap"; // <-- YOU FORGOT THIS LINE
 
 // https://astro.build/config
-export default defineConfig({ vite: { plugins: [tailwindcss()], }, });
-
+export default defineConfig({
+  site: 'https://hachemsqualli.xyz', // This is required for the sitemap to work
+  vite: { 
+    plugins: [tailwindcss()], 
+  },
+  integrations: [
+    icon(),
+    sitemap() // Now this knows what 'sitemap' is
+  ],
+});
